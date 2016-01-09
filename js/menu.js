faf0363dc8df74b363415afcd51b2df441dedb28
taylord65 <taylor.dotsikas@mail.mcgill.ca>
2016-01-09 14:46:18 -0500

    tabs

---
+++ js/menu.js
@@ -0,0 +1,26 @@
+var navLinks = $('.navLink');
+var contentElements = $('.content');
+
+navLinks.on('click', function (e) {
+
+
+    //set menu    
+    navLinks.removeClass('selected');
+   
+    $(e.currentTarget).addClass('selected');
+    var navId = $(e.currentTarget).attr('id');
+
+    //set content
+    contentElements.hide();
+    switch (navId) {
+        case "menu1":
+            $('#one').show();
+            break;
+        case "menu2":
+            $('#two').show();
+            break;
+        case "menu3":
+            $('#three').show();
+            break;
+    }
+});
