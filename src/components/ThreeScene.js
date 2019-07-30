1470572d11b59ba56946d972a762cff614df5b2e
taylordotsikas65 <taylordotsikas@gmail.com>
2019-07-29 20:36:28 -0400

    working but antialiasing is not that good

--- src/components/ThreeScene.js
+++ src/components/ThreeScene.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as THREE from 'three';
-import * as POSTPROCESSING from 'postprocessing';
+import { EffectComposer, EffectPass, RenderPass, GlitchEffect, NoiseEffect } from "postprocessing";
 import * as ORBIT from 'three-orbitcontrols';
 
 class ThreeScene extends React.Component {
@@ -14,45 +14,51 @@ class ThreeScene extends React.Component {
   }
 
   componentDidMount() {
-    const width = window.innerWidth
-    const height = window.innerHeight
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-    const scene = new THREE.Scene()
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000 );
+    const glitch = new GlitchEffect(64);
+    const effectPass = new EffectPass(camera, glitch);
+    effectPass.renderToScreen = true;
 
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      width / height,
-      0.1,
-      1000
-    )
 
-    //CAMERA
-    // camera.position.z = 5061;
-    // camera.position.y = -1144;
+    const clock = new THREE.Clock();
 
-    const renderer = new THREE.WebGLRenderer({ antialias: true })
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0xff00ff })
-    const cube = new THREE.Mesh(geometry, material)
 
-    camera.position.z = 4
-    scene.add(cube)
-    renderer.setClearColor('#000000')
-    renderer.setSize(width, height)
+    const renderer = new THREE.WebGLRenderer({ antialias: true});
+    renderer.setSize(width, height);
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFShadowMap;
 
-    this.scene = scene
-    this.camera = camera
-    this.renderer = renderer
-    this.material = material
-    this.cube = cube
 
-    let composer = new POSTPROCESSING.EffectComposer( renderer );
+    const composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
+    composer.addPass(effectPass);
 
-    this.updateDimensions();
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+    const cube = new THREE.Mesh(geometry, material);
+
+    camera.position.z = 10;
+    scene.add(cube);
+    scene.background = new THREE.Color( 0xFFFFFF );
+
+
+    this.scene = scene;
+    this.camera = camera;
+    this.renderer = renderer;
+    this.material = material;
+    this.cube = cube;
+    this.composer = composer;
+    this.clock = clock;
+
     window.addEventListener("resize", this.updateDimensions.bind(this));
 
-    this.mount.appendChild(this.renderer.domElement)
-    this.start()
+    this.mount.appendChild(this.renderer.domElement);
+    this.start();
   }
 
   componentWillUnmount() {
@@ -76,10 +82,12 @@ class ThreeScene extends React.Component {
     this.cube.rotation.y += 0.01
 
     this.renderScene()
+
     this.frameId = window.requestAnimationFrame(this.animate)
   }
 
   updateDimensions() {
+    //this.composer.setSize(window.innerWidth, window.innerHeight);
     this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(window.innerWidth, window.innerHeight)
@@ -87,6 +95,7 @@ class ThreeScene extends React.Component {
 
   renderScene() {
     this.renderer.render(this.scene, this.camera)
+    this.composer.render(this.clock.getDelta());
   }
 
   render() {
