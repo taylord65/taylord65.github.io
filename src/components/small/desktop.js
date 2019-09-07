d8e406ef5b9a1563a8194d95ed16bca9d6e82328
taylordotsikas65 <taylordotsikas@gmail.com>
2019-09-07 12:32:55 -0400

    react-reveal

--- src/components/small/desktop.js
+++ src/components/small/desktop.js
@@ -3,10 +3,12 @@
 */
 
 import React from "react";
+import Fade from 'react-reveal/Fade'
 
 class Desktop extends React.Component {
   render() {
     return (
+      <Fade big>
       <div className="desktop-video">
         <img src="https://s3.us-east-2.amazonaws.com/taylordotsikasportfolio/macbook2X_noLabel.jpg" alt="Macbook" />
         <div className="video-container">
@@ -15,6 +17,7 @@ class Desktop extends React.Component {
           </video>
         </div>
       </div>
+      </Fade>
     );
   }
 }
--- src/components/small/iphoneVideo.js
+++ src/components/small/iphoneVideo.js
@@ -1,16 +1,19 @@
 import React from "react"
 import { ReactComponent as Iphone } from './iphoneSVG.svg'
+import Fade from 'react-reveal/Fade'
 
 class IphoneVideo extends React.Component {
   render() {
     return (
       <div className="iphone-container">
         <Iphone />
+        <Fade big>
         <div className="video-container">
-          <video loop autoPlay playsInline>
+          <video loop autoPlay muted playsInline>
             <source src={this.props.src} type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
           </video>
         </div>
+        </Fade>
       </div>
     );
   }
