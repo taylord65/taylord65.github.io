1deb96348625cbbffaa4814b131222ac2b9dc402
taylordotsikas65 <taylordotsikas@gmail.com>
2019-09-29 16:42:53 -0400

    removed react reveal from desktop

--- src/components/small/desktop.js
+++ src/components/small/desktop.js
@@ -3,21 +3,17 @@
 */
 
 import React from "react";
-import Fade from 'react-reveal/Fade'
 
 class Desktop extends React.Component {
   render() {
     return (
       <div className="desktop-video">
         <img src="https://s3.us-east-2.amazonaws.com/taylordotsikasportfolio/macbook2X_noLabel.jpg" alt="Macbook" />
-      <Fade big>
         <div className="video-container">
           <video loop autoPlay playsInline>
             <source src={this.props.src} type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
           </video>
         </div>
-      </Fade>
-
       </div>
     );
   }
