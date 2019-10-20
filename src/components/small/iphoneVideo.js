2d3647cf92b343e78c3e45fa3e77c1084f09e3f5
taylordotsikas65 <taylordotsikas@gmail.com>
2019-10-20 16:46:57 -0400

    test falback image on iphone

--- src/components/small/iphoneVideo.js
+++ src/components/small/iphoneVideo.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { ReactComponent as Iphone } from './iphoneSVG.svg'
+import noVideoLoaded from './../../images/error-flat.png'
 
 class IphoneVideo extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class IphoneVideo extends React.Component {
         <div className="video-container">
           <video loop autoPlay muted playsInline>
             <source src={this.props.src} type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
+            <img src={noVideoLoaded} alt="Your browser does not support the <video> tag" />
           </video>
         </div>
       </div>
