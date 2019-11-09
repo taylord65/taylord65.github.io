f082db8573826dc89d0d1279505d2c20aacfefdd
taylordotsikas65 <taylordotsikas@gmail.com>
2019-11-09 15:09:33 -0500

    init alexa ring

--- src/pages/sunlife.js
+++ src/pages/sunlife.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React from "react"
 import SidePanel from './../components/small/sidePanel'
 
 class SunLife extends React.Component {
+
   render() {
     return (
-      <div className="portfolio-feature">
+      <div className="portfolio-feature three-feature">
         <SidePanel routerProps={this.props} />
-	        <div className="animated fadeIn cover" id="s1_background">
-	          <div className="cover-headline">
-	            <h1>Sun Life</h1>
-	            <h2>Development</h2>
-	            <h3>Voice applications</h3>
-	            <h3>Current</h3>
-	          </div>
-	        </div>
+        <div className="animated fadeIn cover slf-cover" id="s1_background">
+          <div className="cover-headline slf-cover-headline">
+            <h1>Sun Life</h1>
+            <h2>Development</h2>
+            <h3>Voice applications</h3>
+            <h3>Current</h3>
+          </div>
+        </div>
+        <div className="animated fadeIn ring-container">
+          <div className="blue-ring">
+            <div className="black-cover"></div>
+          </div>
+        </div>
       </div>
     );
   }
