44ea6194391d35f0920be16062505ac6d5ae220b
taylordotsikas65 <taylordotsikas@gmail.com>
2019-12-12 18:15:22 -0500

    Updates

---
+++ precache-manifest.d38019183cd897961c2cda88572ddb25.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c71529fa3c15111f5da566470bc916",
+    "url": "/taylord65.github.io/index.html"
+  },
+  {
+    "revision": "817fdd2af2b758d580db",
+    "url": "/taylord65.github.io/static/css/main.9d563e77.chunk.css"
+  },
+  {
+    "revision": "398946065625c5b04246",
+    "url": "/taylord65.github.io/static/js/2.82f39cbe.chunk.js"
+  },
+  {
+    "revision": "817fdd2af2b758d580db",
+    "url": "/taylord65.github.io/static/js/main.8fbcbc0c.chunk.js"
+  },
+  {
+    "revision": "d1ad9c1bbaa0eb12c8f7",
+    "url": "/taylord65.github.io/static/js/runtime~main.8b9d6155.js"
+  },
+  {
+    "revision": "5d6673c4275c725f69700efd0dcdbf20",
+    "url": "/taylord65.github.io/static/media/MyFont.5d6673c4.otf"
+  },
+  {
+    "revision": "4df589fedd29fc51223ba3a9f755d61a",
+    "url": "/taylord65.github.io/static/media/fl_cover_dark.4df589fe.jpg"
+  },
+  {
+    "revision": "c3e3def871af83fd9b4386b9467f1d2c",
+    "url": "/taylord65.github.io/static/media/iphoneSVG.c3e3def8.svg"
+  },
+  {
+    "revision": "0855481fea4727f98ae5101a99037826",
+    "url": "/taylord65.github.io/static/media/s1_cover_faded.0855481f.png"
+  },
+  {
+    "revision": "647d1af5d77d0ef37c7ff4be2e7fdd46",
+    "url": "/taylord65.github.io/static/media/sd_cover.647d1af5.jpg"
+  },
+  {
+    "revision": "a1f0ef88bc0ff904df6d047e8a5165d9",
+    "url": "/taylord65.github.io/static/media/tb_new_cover.a1f0ef88.jpg"
+  }
+]);
